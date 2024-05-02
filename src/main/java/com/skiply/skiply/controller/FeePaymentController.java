a77765b409498328021e47e0b92bbbf993cbc2a4
kevin <kevin24260321@gmail.com>
2024-05-02 11:24:51 +0400

    Adding versioning

--- src/main/java/com/skiply/skiply/controller/FeePaymentController.java
+++ src/main/java/com/skiply/skiply/controller/FeePaymentController.java
@@ -6,13 +6,12 @@ import com.skiply.skiply.entity.Receipt;
 import com.skiply.skiply.service.FeePaymentService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequestMapping("/api/v1")
 public class FeePaymentController {
 
     @Autowired
